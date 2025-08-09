3cfcab7f39fc1291c6ed9aa5ae698105981d1e5e
Sathwik Chetan Hanumadh Sai Inuganty <ci137@scarletmail.rutgers.edu>
2025-08-09 19:12:15 -0400

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,12 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/cooklingo/',   // <-- make sure this matches your repo name
   plugins: [react()],
   server: {
     port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:4000',
-        changeOrigin: true
-      }
-    }
+    proxy: { '/api': { target: 'http://localhost:4000', changeOrigin: true } }
   }
 })
+
